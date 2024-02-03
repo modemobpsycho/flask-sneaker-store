63cb45574df7efc744e95ea26d7d84999f9b8b37
Vadim <hawaiianwork@mail.ru>
2024-02-03 14:56:48 +0300

    creating routes/views

--- website/__init__.py
+++ website/__init__.py
@@ -1,3 +1,4 @@
+from re import A
 from flask import Flask
 
 
@@ -5,4 +6,9 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "mrobs59fghdfg"
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
     return app
