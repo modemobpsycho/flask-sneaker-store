31f46c14685bc79acbf3dfa53bb194332febb15f
Vadim <hawaiianwork@mail.ru>
2024-02-03 19:28:02 +0300

    upd: flashing messages & base html update

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 auth = Blueprint("auth", __name__)
 
 
@@ -22,14 +22,14 @@ def sign_up():
         password2 = request.form.get('password2')
 
         if len(email) < 4:
-            pass
+            flash('Email must be greater than 4 characters.', category='error')
         elif len(firstName) < 2:
-            pass
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            pass
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            pass
+            flash('Password must be at least 7 characters.', category='error')
         else:
             # database add
-            pass
+            flash('Account created!', category='success')
     return render_template("sign_up.html")
