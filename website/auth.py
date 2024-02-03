80b8664e854e6b801d93adce9592c619ecfcff5f
Vadim <hawaiianwork@mail.ru>
2024-02-03 18:11:04 +0300

    jinja templating & html

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login")
 def login():
-    return "<p>Login</p>"
+    return render_template('login.html')
 
 
 @auth.route("/logout")
@@ -15,4 +15,4 @@ def logout():
 
 @auth.route("/sign-up")
 def sign_up():
-    return "<p>Sign up</p>"
+    return render_template("sign_up.html")
