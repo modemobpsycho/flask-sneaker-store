2ea5e6976b968002d32e5f2180dbfadb9b23d533
Vadim <hawaiianwork@mail.ru>
2024-02-06 22:26:50 +0300

    upd: db models

---
+++ website/models.py
@@ -0,0 +1,17 @@
+from email.policy import default
+
+from sqlalchemy import func
+from . import db
+from flask_login import UserMixin
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone = True), default = func.now())    
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
