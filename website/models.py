b39a89fb2fa4f407942106ac32365106d4fd6af5
Vadim <hawaiianwork@mail.ru>
2024-02-07 12:20:08 +0300

    add: new models

--- website/models.py
+++ website/models.py
@@ -3,11 +3,32 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500))
+    price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String(200))
+
+    sizes = db.relationship('Size', backref='product',
+                            cascade='all, delete-orphan')
+
+
+class Size(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'product.id'), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    available = db.Column(db.Boolean, nullable=False, default=True)
+
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'product.id'), nullable=False)
+    size_id = db.Column(db.Integer, db.ForeignKey('size.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
 
 
 class User(db.Model, UserMixin):
@@ -15,4 +36,5 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    cart_items = db.relationship(
+        'CartItem', backref='user', cascade='all, delete-orphan')
