07335d13563c9776f2766815d0e1d06c63bcc472
Vadim <hawaiianwork@mail.ru>
2024-02-09 17:44:56 +0300

    fix: sign up with old-new user

--- website/auth.py
+++ website/auth.py
@@ -58,7 +58,7 @@ def sign_up():
                             password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for("views.home"))
 
