b9ba358dd8c9ecc36fc761094789b24c24eae5f8
Vadim <hawaiianwork@mail.ru>
2024-02-12 19:39:16 +0300

    upd: creating your own session models for each individual user and working on fav

--- website/models.py
+++ website/models.py
@@ -1,6 +1,16 @@
-from . import db
+from datetime import datetime
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from . import db
+
+
+class CustomSessionModel(db.Model):
+    __tablename__ = "sessions"
+
+    session_id = db.Column(db.String(255), primary_key=True)
+    data = db.Column(db.Text)
+    expiry = db.Column(db.DateTime)
+    modified = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 class Product(db.Model):
@@ -10,25 +20,47 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     image_url = db.Column(db.String(200))
 
-    sizes = db.relationship('Size', backref='product',
-                            cascade='all, delete-orphan')
+    sizes = db.relationship("Size", backref="product", cascade="all, delete-orphan")
+
+    favorites = db.relationship(
+        "User", secondary="favorites", backref=db.backref("favorites", lazy="dynamic")
+    )
 
 
 class Size(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey(
-        'product.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
     size = db.Column(db.Integer, nullable=False)
     available = db.Column(db.Boolean, nullable=False, default=True)
 
 
 class CartItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey(
-        'product.id'), nullable=False)
-    size_id = db.Column(db.Integer, db.ForeignKey('size.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+    size_id = db.Column(db.Integer, db.ForeignKey("size.id"), nullable=False)
     quantity = db.Column(db.Integer, nullable=False, default=1)
+
+
+class Favorites(db.Model):
+    __tablename__ = "favorites"
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship(
+        "User", backref=db.backref("favorites_assoc", cascade="all, delete-orphan")
+    )
+
+    product = db.relationship(
+        "Product", backref=db.backref("users_assoc", cascade="all, delete-orphan")
+    )
+
+
+class UserFavoritesCount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True)
+    favorites_count = db.Column(db.Integer, default=0)
 
 
 class User(db.Model, UserMixin):
@@ -36,5 +68,19 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    cart_items = db.relationship(
-        'CartItem', backref='user', cascade='all, delete-orphan')
+    favorite_products = db.relationship(
+        "Product", secondary="favorites", backref=db.backref("users", lazy="dynamic")
+    )
+
+    def add_to_favorites(self, product):
+        if product not in self.favorite_products:
+            self.favorite_products.append(product)
+
+    def has_favorited(self, product):
+        return product in self.favorites
+
+    def get_favorites_count(self):
+        return Favorites.query.filter_by(user_id=self.id).count()
+
+    def get_favorites(self):
+        return self.favorites.all()
--- website/views.py
+++ website/views.py
@@ -1,77 +1,123 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import login_required, current_user, user_accessed
-from .models import Product, Size
+from flask import Blueprint, flash, redirect, render_template, request, url_for, session
+from flask_login import login_required, current_user
+from sqlalchemy import func
+from .models import Favorites, Product, Size, UserFavoritesCount
 from . import db
+
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 @login_required
 def home():
-    return render_template('base.html', user=current_user)
+    return render_template("base.html", user=current_user)
 
 
-@views.route('/cart')
+@views.route("/cart")
 @login_required
 def cart():
-    return render_template('cart.html', user=current_user)
+    return render_template("cart.html", user=current_user)
 
 
-@views.route('/order')
+@views.route("/favorites")
+@login_required
+def favorites():
+    favorites_count = current_user.get_favorites_count()
+    favorites = Favorites.query.filter_by(user_id=current_user.id).all()
+    user_favorites = []
+
+    for favorite in favorites:
+        product = Product.query.get(favorite.product_id)
+        user_favorites.append(product)
+
+    return render_template(
+        "favorites.html",
+        favorites_count=favorites_count,
+        user_favorites=user_favorites,
+        user=current_user,
+    )
+
+
+@views.route("/order")
 @login_required
 def order():
-    return render_template('order.html', user=current_user)
+    return render_template("order.html", user=current_user)
 
 
-@views.route('/product')
+@views.route("/product")
 @login_required
 def product_page():
     products = Product.query.all()
     sizes = Size.query.all()
-    return render_template('product.html', products=products, sizes=sizes, user=current_user)
+    return render_template(
+        "product.html", products=products, sizes=sizes, user=current_user
+    )
 
 
-@views.route('/checkout')
+@views.route("/checkout")
 @login_required
 def checkout():
-    return render_template('checkout.html', user=current_user)
+    return render_template("checkout.html", user=current_user)
 
 
-@views.route('/category')
+@views.route("/category")
 def category():
-    return render_template('category.html', user=current_user)
+    return render_template("category.html", user=current_user)
 
 
-@views.route('/add-to-cart', methods=['GET'])
+@views.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "POST":
+        search_query = request.form.get("search_query")
+        products = Product.query.filter(
+            func.lower(Product.name).like(f"%{search_query.lower()}%")
+        ).all()
+        return render_template(
+            "search_results.html", products=products, query=search_query
+        )
+    else:
+        return render_template("search.html", user=current_user)
+
+
+@views.route("/add-to-cart", methods=["GET"])
 @login_required
 def add_to_cart():
-    product_id = request.args.get('product_id')
+    product_id = request.args.get("product_id")
 
-    return redirect(url_for('views.cart'))
+    return redirect(url_for("views.cart"))
 
 
-@views.route('/add-to-favorites', methods=['GET', 'POST'])
+@views.route("/add-to-favorites", methods=["POST"])
 @login_required
 def add_to_favorites():
-    if request.method == "POST":
-        product_id = request.form.get('product_id')
+    product_id = request.form.get("product_id")
+    product_id = int(product_id)
 
-        if product_id:
-            try:
-                product_id = int(product_id)
-            except ValueError:
-                flash('Неправильный идентификатор товара!', 'error')
-                return redirect(request.referrer or url_for('views.home'))
+    product = Product.query.get(product_id)
 
-            product = Product.query.get(product_id)
+    if product:
+        if not current_user.has_favorited(product):
+            current_user.add_to_favorites(product)
+            db.session.commit()
 
-            if product:
-                current_user.add_to_favorites(product)
-                db.session.commit()
-                flash('Товар успешно добавлен в избранные!', 'success')
+            user_favorites_count = UserFavoritesCount.query.filter_by(
+                user_id=current_user.id
+            ).first()
+            if user_favorites_count:
+                user_favorites_count.favorites_count += 1
             else:
-                flash('Товар не найден!', 'error')
-        else:
-            flash('Неправильный идентификатор товара!', 'error')
+                user_favorites_count = UserFavoritesCount(
+                    user_id=current_user.id, favorites_count=1
+                )
+                db.session.add(user_favorites_count)
 
-    return redirect(request.referrer or url_for('views.cart'))
+            db.session.commit()
+
+            session["favorites_count"] = user_favorites_count.favorites_count
+
+        else:
+            flash("Product already exist!", "info")
+    else:
+        flash("Product is undefined!", "error")
+
+    return redirect(request.referrer or url_for("views.favorites"))
