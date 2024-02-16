af490ad0bc84f35a0af3c21f52a7209145b3d763
Vadim <hawaiianwork@mail.ru>
2024-02-16 23:34:39 +0300

    feat: add errorhandler for 404

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_login import current_user
 from website import create_app
 
 app: Flask = create_app()
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return (
+        render_template("page404.html", user=current_user),
+        404,
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
