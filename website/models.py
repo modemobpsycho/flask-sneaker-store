a2828f7b5a9e56d6cab33c4e8a007592bce08008
Vadim <hawaiianwork@mail.ru>
2024-02-22 03:00:39 +0300

    new column for feat with mail

--- website/models.py
+++ website/models.py
@@ -80,6 +80,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    access = db.Column(db.Boolean, default=False)
     favorite_products = db.relationship(
         "Product", secondary="favorites", backref=db.backref("users", lazy="dynamic")
     )
